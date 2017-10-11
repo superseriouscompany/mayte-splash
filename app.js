294f6c9843ea87d775b7d41cea9f3a5e48043c90
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-10-11 17:42:42 -0400

    Basic UI functionality with dummy buttons

--- app.js
+++ app.js
@@ -9034,7 +9034,33 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 "use strict";
 
 
-console.log("app");
+var $ = document.querySelector.bind(document);
+
+$('form').addEventListener('submit', submit);
+$('.js-instagram').addEventListener('click', instagramAuth);
+$('.js-linkedin').addEventListener('click', linkedinAuth);
+
+function submit(evt) {
+  if ($('.js-promo').value.toLowerCase() !== 'treats!') {
+    showError('The promo code is incorrect');
+  }
+  document.querySelectorAll('.step').forEach(function (el) {
+    el.style.display = 'none';
+  });
+  $('.step.two').style.display = 'block';
+}
+
+function instagramAuth() {
+  console.log('instagram');
+}
+
+function linkedinAuth() {
+  console.log('linkedin');
+}
+
+function showError(msg) {
+  return alert(msg);
+}
 
 /***/ }),
 /* 329 */
