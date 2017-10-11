a204e626c28fa3d136c197ff94a80a25d39d3d9c
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-10-11 19:02:06 -0400

    Fix bug with incorrect promo code

--- src/js/app.js
+++ src/js/app.js
@@ -17,7 +17,7 @@ if( accessToken = qs('at') ) {
 
 function submit(evt) {
   if( $('.step.one').style.display === 'block' ) {
-    if( $('.js-promo').value.toLowerCase() !== 'treats!' ) { showError('The promo code is incorrect') }
+    if( $('.js-promo').value.toLowerCase() !== 'treats!' ) { return showError('The promo code is incorrect') }
     return showStep('two')
   }
 
